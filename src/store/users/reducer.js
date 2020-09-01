7a5b4292151b1b3065e69f499d0e12d2b86b8c4e
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2020-09-01 13:18:17 +0400

    reducer for users and init redux

---
+++ src/store/users/reducer.js
@@ -0,0 +1,35 @@
+import { userActionTypes } from "./actionTypes";
+
+const initialState = {
+    list: Object.create(null),
+    ids: [],
+    filter: '',
+};
+
+const reducerMapping = {
+    [userActionTypes.getAll]: (state, users) => {
+        const list = Object.create(null);
+        users.forEach(user => list[user.id] = user);
+
+        return {
+            ...state,
+            list,
+            ids: users.map(user => user.id)
+        };
+    },
+
+    [userActionTypes.delete]: (state, id) => {
+        const list = { ...state.list };
+        delete list[id];
+        return {
+            ...state,
+            list,
+            ids: state.ids.filter(x => x !== id)
+        };
+    },
+
+    [userActionTypes.applyFilter]: (state, filter) => ({ ...state, filter }),
+
+}
+
+export const users = (state = initialState, action) => reducerMapping[action.type] ? reducerMapping[action.type](state, action.payload) : state;
