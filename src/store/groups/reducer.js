50e738c663bf090aff5476b4fdc4e279fb2b9dff
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2020-09-10 20:45:18 +0400

    code format and fix json

--- src/store/groups/reducer.js
+++ src/store/groups/reducer.js
@@ -1,28 +1,23 @@
-import {groupActionTypes} from './actionTypes';
-
-const _groupShape = {
-  name: 'String',
-  users: 'Array',
-};
+import { groupActionTypes } from "./actionTypes";
 
 const initialState = {
-  list: Object.create (null),
+  list: Object.create(null),
   ids: [],
-  filter: '',
+  filter: "",
 };
 
 const reducerMapping = {
   [groupActionTypes.delete]: (state, id) => {
-    const list = {...state.list};
+    const list = { ...state.list };
     delete list[id];
     return {
       ...state,
       list,
-      ids: state.ids.filter (x => x !== id),
+      ids: state.ids.filter((x) => x !== id),
     };
   },
 
-  [groupActionTypes.create]: (state, {name, id}) => {
+  [groupActionTypes.create]: (state, { name, id }) => {
     return {
       ...state,
       list: {
@@ -36,7 +31,7 @@ const reducerMapping = {
     };
   },
 
-  [groupActionTypes.addUserToGroup]: (state, {idGroup, idUser}) => {
+  [groupActionTypes.addUserToGroup]: (state, { idGroup, idUser }) => {
     return {
       ...state,
       list: {
@@ -49,23 +44,23 @@ const reducerMapping = {
     };
   },
 
-  [groupActionTypes.removeUserFromGroup]: (state, {idGroup, idUser}) => {
+  [groupActionTypes.removeUserFromGroup]: (state, { idGroup, idUser }) => {
     return {
       ...state,
       list: {
         ...state.list,
         [idGroup]: {
           ...state.list[idGroup],
-          users: state.state.list[idGroup].filter (x => x !== idUser),
+          users: state.state.list[idGroup].filter((x) => x !== idUser),
         },
       },
     };
   },
 
-  [groupActionTypes.applyFilter]: (state, filter) => ({...state, filter}),
+  [groupActionTypes.applyFilter]: (state, filter) => ({ ...state, filter }),
 };
 
 export const groups = (state = initialState, action) =>
   reducerMapping[action.type]
-    ? reducerMapping[action.type] (state, action.payload)
+    ? reducerMapping[action.type](state, action.payload)
     : state;
