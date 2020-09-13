45a7b11e0a65814ff14fe5aa61844d2c66c88a60
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2020-09-13 15:23:59 +0400

    fix mistype

--- src/store/groups/actions.js
+++ src/store/groups/actions.js
@@ -1,31 +1,27 @@
+import { groupActionTypes } from "./actionTypes";
+import { v4 as uuidv4 } from "uuid";
 
-import { groupActionTypes } from "./actionTypes"
-import { v4 as uuidv4 } from 'uuid';
-
-
-
-export const applyFilter = filter => ({
-    type: groupActionTypes.applayFilter,
-    payload: filter
+export const applyFilter = (filter) => ({
+  type: groupActionTypes.applyFilter,
+  payload: filter,
 });
 
-
-export const deleteGroup = id => ({
-    type: groupActionTypes.delete,
-    payload: id
+export const deleteGroup = (id) => ({
+  type: groupActionTypes.delete,
+  payload: id,
 });
 
-export const createGroup = name => ({
-    type: groupActionTypes.create,
-    payload: {name, id: uuidv4()}
+export const createGroup = (name) => ({
+  type: groupActionTypes.create,
+  payload: { name, id: uuidv4() },
 });
 
-export const addUserToGroup = ({idGroup, idUser}) => ({
-    type: groupActionTypes.addUserToGroup,
-    payload: {idGroup, idUser}
+export const addUserToGroup = ({ idGroup, idUser }) => ({
+  type: groupActionTypes.addUserToGroup,
+  payload: { idGroup, idUser },
 });
 
-export const removeUserFromGroup = ({idGroup, idUser}) => ({
-    type: groupActionTypes.removeUserFromGroup,
-    payload: {idGroup, idUser}
+export const removeUserFromGroup = ({ idGroup, idUser }) => ({
+  type: groupActionTypes.removeUserFromGroup,
+  payload: { idGroup, idUser },
 });
